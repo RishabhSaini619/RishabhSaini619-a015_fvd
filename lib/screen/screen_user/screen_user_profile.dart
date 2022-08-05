0a89a5b4680ec522187ac91982e27c849e7a06cc
rishabhsaini619 <rishabhsaini619@gmail.com>
2022-08-05 16:23:48 +0530

    userscree app bar

--- lib/screen/screen_user/screen_user_profile.dart
+++ lib/screen/screen_user/screen_user_profile.dart
@@ -1,8 +1,6 @@
-import 'package:a015_fvd/global/global_theme.dart';
 import 'package:a015_fvd/global/global_variable.dart';
-import 'package:a015_fvd/screen/screen_auth/screen_user_update/screen_user_update.dart';
 import 'package:a015_fvd/screen/screen_auth/widget_user_exit/widget_user_exit.dart';
-import 'package:a015_fvd/widget/widget_text_widget.dart';
+import 'package:a015_fvd/screen/screen_user/screen_user_profile_components.dart';
 import 'package:flutter/material.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -26,59 +24,14 @@ class _UserProfileScreenState extends State<UserProfileScreen>
 
   @override
   Widget build(BuildContext context) {
-    ThemeData themeData = Theme.of(context);
-    return Center(
-      child: Column(
-        children: [
-          InkWell(
-            child: Row(
-              children: [
-                Icon(Icons.manage_accounts),
-                TextWidget(
-                  tText: "Profile Update",
-                  tColor: themeData.primaryColor,
-                  tSize: 20,
-                )
-              ],
-            ),
-            onTap: () async {
-              return Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => UserUpdateScreen(),
-                ),
-              );
-            },
-          ),
-          SwitchListTile(
-            title: TextWidget(
-              isTitle: true,
-              tText: darkMode ? "Dark Theme" : " Light Theme",
-              tColor: themeData.colorScheme.primary,
-              tSize: 24,
-            ),
-            secondary: Icon(
-              darkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
-            ),
-            activeColor: Colors.white,
-            activeTrackColor: Colors.blue,
-            activeThumbImage: const AssetImage(
-              "assets/darkMode/moonIcon.png",
-            ),
-            autofocus: true,
-            inactiveThumbColor: Colors.white,
-            inactiveTrackColor: Colors.grey,
-            inactiveThumbImage: const AssetImage("assets/darkMode/sunIcon.png"),
-            onChanged: (bool value) {
-              setState(() {
-                darkMode = value;
-              });
-            },
-            value: darkMode,
-          ),
-          UserExit(),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        UserProfileScreenAppBarImage(),
+        UserProfileScreenUpdateProfileButton(),
+        UserProfileScreenThemeUiSwitch(),
+        UserExit(),
+      ],
     );
   }
 }
---
+++ lib/screen/screen_user/screen_user_profile_components.dart
@@ -0,0 +1,79 @@
+import 'package:a015_fvd/global/global_theme.dart';
+import 'package:a015_fvd/screen/screen_auth/screen_user_update/screen_user_update.dart';
+import 'package:a015_fvd/widget/widget_text_widget.dart';
+import 'package:flutter/material.dart';
+
+class UserProfileScreenAppBarImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      fit: BoxFit.fitHeight,
+      image: AssetImage('assets/appbar/user.png'),
+    );
+  }
+}
+class UserProfileScreenUpdateProfileButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return InkWell(
+      child: Row(
+        children: [
+          Icon(Icons.manage_accounts),
+          TextWidget(
+            tText: "Profile Update",
+            tColor: themeData.primaryColor,
+            tSize: 20,
+          )
+        ],
+      ),
+      onTap: () async {
+        return Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => UserUpdateScreen(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class UserProfileScreenThemeUiSwitch extends StatefulWidget {
+  @override
+  State<UserProfileScreenThemeUiSwitch> createState() => _UserProfileScreenThemeUiSwitchState();
+}
+
+class _UserProfileScreenThemeUiSwitchState extends State<UserProfileScreenThemeUiSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return SwitchListTile(
+      title: TextWidget(
+        isTitle: true,
+        tText: darkMode ? "Dark Theme" : " Light Theme",
+        tColor: themeData.colorScheme.primary,
+        tSize: 24,
+      ),
+      secondary: Icon(
+        darkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+      ),
+      activeColor: Colors.white,
+      activeTrackColor: Colors.blue,
+      activeThumbImage: const AssetImage(
+        "assets/darkMode/moonIcon.png",
+      ),
+      autofocus: true,
+      inactiveThumbColor: Colors.white,
+      inactiveTrackColor: Colors.grey,
+      inactiveThumbImage: const AssetImage("assets/darkMode/sunIcon.png"),
+      onChanged: (bool value) {
+        setState(() {
+          darkMode = value;
+        });
+      },
+      value: darkMode,
+    );
+  }
+}
